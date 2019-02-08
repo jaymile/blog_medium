818eedb833b6c069ce3293d8b8e8169076228e5c
jaymile <djemil.aminou@gmail.com>
2019-02-08 19:11:44 +0100

    chapitre 12

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'pages#index'
+  get 'about', to: 'pages#About'
+  resources :articles
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
